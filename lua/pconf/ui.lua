c455174faf73fdb3edd6e26828218c8703ad1ff1
yuchanns <airamusume@gmail.com>
2021-08-30 01:43:53 +0800

    hide the tilde sign on blank lines

--- lua/pconf/ui.lua
+++ lua/pconf/ui.lua
@@ -190,3 +190,5 @@ require'treesitter-context'.setup{
     enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
     throttle = true, -- Throttles plugin updates (may improve performance)
 }
+
+vim.cmd("let &fcs='eob: '")
