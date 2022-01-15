5d460cda89bb24756d2f5a187ade53ff3a850e25
yuchanns <airamusume@gmail.com>
2022-01-15 19:47:16 +0800

    update packer config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,8 @@
+local fn = vim.fn
 local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.cmd('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
-  vim.cmd 'packadd packer.nvim'
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
 return require('packer').startup(function ()
@@ -76,4 +76,8 @@ return require('packer').startup(function ()
   use 'tpope/vim-dadbod'
   use 'kristijanhusak/vim-dadbod-ui'
   use 'kristijanhusak/vim-dadbod-completion'
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
