0bfd4842d67c641d755d808c47af061e4457ef44
yuchanns <airamusume@gmail.com>
2022-01-26 10:34:35 +0800

    add executable check for language client

--- lua/setup/lspconfig.lua
+++ lua/setup/lspconfig.lua
@@ -4,6 +4,8 @@ if not status_ok then
   return
 end
 
+local executable = vim.fn.executable
+
 local nvim_lsp = require('lspconfig')
 
 -- Use an on_attach function to only map the following keys
@@ -28,94 +30,104 @@ local on_attach = function(client, bufnr)
 end
 
 -- golang
-nvim_lsp["gopls"].setup {
-  on_attach = on_attach,
-  flags = {
-    debounce_text_changes = 150,
-  },
-  settings = {
-    gopls = {
-      analyses = { composites = false }
-    }
-  }
-}
-
-require('go').setup {
-    auto_format = true,
-    auto_lint = true,
-    linter = 'revive',
-    lint_prompt_style = 'vt',
-    formatter = 'goimports',
-}
-
--- rust
-local rust_tools_opt = {
-  server = {
+if executable('gopls') > 0 then
+  nvim_lsp['gopls'].setup {
     on_attach = on_attach,
     flags = {
       debounce_text_changes = 150,
     },
-    assist = {
-      importMergeBehavior = "last",
-      importPrefix = "by_self",
-    },
-    diagnostics = {
-      disabled = { "unresolved-import" }
-    },
-    cargo = {
-      loadOutDirsFromCheck = true
-    },
-    procMacro = {
-      enable = true
-    },
-    checkOnSave = {
-      command = "clippy"
-    },
+    settings = {
+      gopls = {
+        analyses = { composites = false }
+      }
+    }
   }
-}
+  local opts = { lint_prompt_style = 'vt' }
+  if executable('revive') > 0 then
+    opts.auto_lint = true
+    opts.linter = 'revive'
+  end
+  if executable('goimports') > 0 then
+    opts.auto_format = true
+    opts.formatter = 'goimports'
+  end
+  require('go').setup(opts)
+end
 
-require('rust-tools').setup(rust_tools_opt)
-vim.g.rustfmt_autosave = 1
+-- rust
+if executable('rust-analyzer') > 0 then
+  local rust_tools_opt = {
+    server = {
+      on_attach = on_attach,
+      flags = {
+        debounce_text_changes = 150,
+      },
+      assist = {
+        importMergeBehavior = "last",
+        importPrefix = "by_self",
+      },
+      diagnostics = {
+        disabled = { "unresolved-import" }
+      },
+      cargo = {
+        loadOutDirsFromCheck = true
+      },
+      procMacro = {
+        enable = true
+      },
+      checkOnSave = {
+        command = "clippy"
+      },
+    }
+  }
+
+  require('rust-tools').setup(rust_tools_opt)
+  vim.g.rustfmt_autosave = 1
+end
 
 -- lua
 -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
 local sumneko_root_path = vim.fn.stdpath('data')..'/lspconfig/sumneko_lua/lua-language-server'
 local sumneko_binary = sumneko_root_path.."/bin/lua-language-server"
+if executable(sumneko_binary) > 0 then
+  local runtime_path = vim.split(package.path, ';')
+  table.insert(runtime_path, "lua/?.lua")
+  table.insert(runtime_path, "lua/?/init.lua")
 
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
-nvim_lsp["sumneko_lua"].setup {
-  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = runtime_path,
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
+  nvim_lsp["sumneko_lua"].setup {
+    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+          -- Setup your lua path
+          path = runtime_path,
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
       },
     },
-  },
-}
+  }
+end
+
 -- php
 vim.cmd('autocmd FileType php set iskeyword+=$')
 local phpactor_root_path = vim.fn.stdpath('data')..'/lspconfig/phpactor'
 local phpactor_binary = phpactor_root_path.."/bin/phpactor"
-nvim_lsp["phpactor"].setup {
+if executable(phpactor_binary) > 0 then
+  nvim_lsp["phpactor"].setup {
     on_attach = on_attach,
     cmd = {phpactor_binary, "language-server", "-vvv"}
-}
+  }
+end
